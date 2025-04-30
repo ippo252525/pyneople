4ea620fdd26074eb388f0989d02634db0c274977
sungpil <ippo252525@gmail.com>
2025-04-30 15:35:24 +0900

    Refactor: endpoint mapping class

--- src/pyneople/api/data_path_map.py
+++ src/pyneople/api/data_path_map.py
@@ -1,6 +1,6 @@
 def standardize_data_map(data_map):
     data_map = {k: v if isinstance(v, tuple) else (v,) for k, v in data_map.items()}
-    data_map.update(fetched_at = 'fetched_at')
+    data_map.update(fetched_at = ('fetched_at',))
     return data_map
 
 CHARACTER_FAME_DATA_PATH_MAP = {
@@ -35,6 +35,8 @@ CHARACTER_INFO_DATA_PATH_MAP = standardize_data_map(CHARACTER_INFO_DATA_PATH_MAP
 
 
 CHARACTER_TIMELINE_DATA_PATH_MAP = {
+    'server_id': 'serverId',
+    'character_id': 'characterId',    
     'timeline_code': 'code',
     'timeline_name': 'name',
     'timeline_date': 'date',
